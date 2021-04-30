e5beae97d3b9516ff476bab00327c29e0223fe2e
Sandeep <deinsangwan@gmail.com>
2021-04-30 22:41:47 +0200

    deployed

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import axios from './axios';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import Video from './Video'
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchPosts() {
+      const response = await axios.get('/v2/posts');
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts();
+
+  },[]);
+
+  console.log(videos);
+
+
+
+
   return (
     // BEM naming convention
     <div className="app">
-      
+
       <div className="app__videos">
-        <Video 
+        {videos.map(({ url, channel, description, song, likes, messages, shares }) => (
+          <Video
+            url={url}
+            channel={channel}
+            song={song}
+            likes={likes}
+            messages={messages}
+            description={description}
+            shares={shares}
+          />
+        )
+        )}
+
+         <Video 
         channel='ssssandeep'
         description='Yooo Tiki Toki'
         song='dhink chik dhink chik'
